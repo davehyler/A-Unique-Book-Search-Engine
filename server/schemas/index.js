1ae3b0a7d449f2960910f37520fe3c0feaa65278
davehyler <164968520+davehyler@users.noreply.github.com>
2024-09-27 20:57:34 -0700

    Delete server directory

--- server/schemas/index.js
+++
@@ -1,6 +0,0 @@
-//See module 21 for code below (import+export typedefs and resolvers)
-
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
-
-module.exports = { typeDefs, resolvers };
